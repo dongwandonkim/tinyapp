085a482244a58f2b199d414f34efebef0d9872f2
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-17 16:54:21 -0700

    login/logout cookie setup

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const bp = require('body-parser');
+const cookieParser = require('cookie-parser');
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
 app.use(bp.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
@@ -22,6 +24,16 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
@@ -31,9 +43,15 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  console.log(req.cookies);
+  const templateVars = {
+    username: req.cookies['username'],
+    urls: urlDatabase,
+  };
+
   res.render('urls_index', templateVars);
 });
+
 app.post('/urls', (req, res) => {
   const {longURL} = req.body;
   const shortURL = generateRandomString();
@@ -42,7 +60,12 @@ app.post('/urls', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies['username'],
+    // ... any other vars
+  };
+
+  res.render('urls_new', templateVars);
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
@@ -69,6 +92,7 @@ app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies['username'],
   };
   res.render('urls_show', templateVars);
 });
