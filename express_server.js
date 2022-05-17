5a40da62c704109acd2f37b5ea83431d2bd051ea
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-16 17:18:54 -0700

    generate random strings for url shortener, Added new url view

--- express_server.js
+++ express_server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const bp = require('body-parser');
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
+app.use(bp.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = () =>{
+  let r = (Math.random() + 1).toString(36).substring(7);
+  return r;
 };
 
 app.get("/", (req, res) => {
@@ -25,11 +33,20 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("urls post req");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
